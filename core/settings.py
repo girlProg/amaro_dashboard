a1251acc3b8179bc751f915c74ce7a872a259d75
girlprog <f.galaudu@gmail.com>
2021-12-11 21:40:21 +0100

    logging

--- core/settings.py
+++ core/settings.py
@@ -149,3 +149,30 @@ STATICFILES_DIRS = (
 
 #############################################################
 #############################################################
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'debug': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+            'formatter': 'debug',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
